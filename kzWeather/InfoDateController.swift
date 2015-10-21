2157e4cfedaf4cac28038648ea110efc6385df65
Vinh Hua <vinhhua@vinhs-mac.local>
2015-10-20 20:43:41 -0700

    sdf

--- kzWeather/InfoDateController.swift
+++ kzWeather/InfoDateController.swift
@@ -10,8 +10,6 @@ import UIKit
 
 class InfoDateController: UIView {
     
-    
-    /*
     // MARK: Initialization
     required init?(coder aDecoder: NSCoder){
         super.init(coder: aDecoder)
@@ -24,84 +22,4 @@ class InfoDateController: UIView {
         let width = Int(frame.size.width)
         return CGSize(width: width, height: height)
     }
-    */
-    
-    // MARK: Properties
-    var rating = 0 {
-        didSet {
-            // Update layout everytime the rate change
-            setNeedsLayout()
-        }
-    }
-    
-    var ratingButtons = [UIButton]()
-    
-    var spacing = 5
-    var stars = 1
-    
-    // MARK: Initialization
-    required init?(coder aDecoder: NSCoder){
-        super.init(coder: aDecoder)
-        
-        
-        let filledStarImage = UIImage(named: "filledStar")
-        
-        let emptyStarImage = UIImage(named: "emptyStart")
-        
-        for _ in 0..<stars {
-            //let button = UIButton(frame: CGRect(x: 0, y: 0, width: 44, height: 44))
-            let button = UIButton()
-            
-            button.setImage(emptyStarImage, forState: .Normal)
-            button.setImage(filledStarImage, forState: .Selected)
-            button.setImage(filledStarImage, forState: [.Highlighted, .Selected])
-            
-            // button.backgroundColor = UIColor.redColor()
-            button.adjustsImageWhenHighlighted = false
-            
-            button.addTarget(self, action: "ratingButtonTapped:", forControlEvents: .TouchDown)
-            
-            ratingButtons += [button]
-            
-            addSubview(button)
-        }
-        
-        
-    }
-    
-    override func layoutSubviews() {
-        // Set the button's width and height to a square the size of the frame's height.
-        let buttonSize = Int(frame.size.height) - 10
-        
-        var buttonFrame = CGRect(x: 0, y: 0, width: buttonSize, height: buttonSize)
-        
-        // Offset each button's origin by the length of the button plus spacing.
-        for (index, button) in ratingButtons.enumerate() {
-            buttonFrame.origin.x = CGFloat(index * (buttonSize + spacing))
-            button.frame = buttonFrame
-        }
-        
-        updateButtonSelectionStates()
-    }
-    
-    override func intrinsicContentSize() -> CGSize {
-        let buttonSize = Int(frame.size.height)
-        let width = (buttonSize + spacing) * stars
-        return CGSize(width: width, height: buttonSize)
-    }
-    
-    func ratingButtonTapped(button: UIButton) {
-        print("Button pressed")
-        rating = ratingButtons.indexOf(button)! + 1
-        
-        updateButtonSelectionStates()
-    }
-    
-    func updateButtonSelectionStates() {
-        for (index, button) in ratingButtons.enumerate() {
-            // If the index of a button is less than the rating, that button should be selected.
-            button.selected = index < rating
-        }
-    }
-
 }
