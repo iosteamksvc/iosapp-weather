f977afad8c761b533af1686fec3d430ed955d77d
BACKUP-PC <backup-pc@backup-pcs-macbook-pro.local>
2015-10-22 13:18:27 +0700

    utils

---
+++ kzWeather/Utils.swift
@@ -0,0 +1,15 @@
+//
+//  Utils.swift
+//  kzWeather
+//
+//  Created by BACKUP-PC on 10/22/15.
+//  Copyright © 2015 iosteamksvc. All rights reserved.
+//
+
+import Foundation
+
+extension Double {
+    func roundToInt() -> Int{
+        return Int(round(self))
+    }
+}
