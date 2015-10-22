6fb476ee9c61b453e1688fc962337ec960324b5c
BACKUP-PC <backup-pc@backup-pcs-macbook-pro.local>
2015-10-22 13:18:57 +0700

    controller

--- kzWeather/ViewController.swift
+++ kzWeather/ViewController.swift
@@ -9,31 +9,25 @@
 import UIKit
 import LatoFont
 import WeatherIconsKit
+import Cartography
 
 class ViewController: UIViewController {
-
-    @IBOutlet weak var lblWeather: UILabel!
     
-    @IBOutlet weak var lblIcon: UILabel!
-    
-    @IBOutlet weak var lblCurrentTemp: UILabel!
-    
-    @IBOutlet weak var lblMinTemp: UILabel!
-    
-    @IBOutlet weak var lblMaxTemp: UILabel!
-    
-    @IBOutlet weak var lblCity: UILabel!
-    
-    @IBOutlet weak var svWeatherHourly: UIScrollView!
+    private let currentWeatherView = CurrentWeatherView(frame: CGRectZero)
+    private let hourlyForecastView = WeatherHourlyForecastView(frame: CGRectZero)
+    private let daysForecastView = WeatherDaysForecastView(frame: CGRectZero)
     
     private var locationService: LocationService?
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        styleCurrentWeatherView()
-        renderCurrentWeatherView()
-        svWeatherHourly.contentSize.width = 400
+        
+        self.view.addSubview(currentWeatherView)
+        self.view.addSubview(hourlyForecastView)
+        self.view.addSubview(daysForecastView)
+        
+        layoutView()
+
     }
 
     override func didReceiveMemoryWarning() {
@@ -48,58 +42,62 @@ class ViewController: UIViewController {
         
         locationService = LocationService() { [weak self] location in
         
-        weatherDatastore.retrieveCurrentWeatherAtLat(location.lat, lon: location.lon) {
-            currentWeatherConditions in
-                self?.renderCurrent(currentWeatherConditions)
+            weatherDatastore.retrieveCurrentWeatherAtLat(location.lat, lon: location.lon) {
+                currentWeatherConditions in
+                    self?.renderCurrent(currentWeatherConditions)
+                    return
+            }
+            
+            weatherDatastore.retrieveHourlyForecastAtLat(location.lat, lon: location.lon) {
+                hourlyWeatherConditions in
+                    self?.renderHourly(hourlyWeatherConditions)
+                    return
+            }
+            weatherDatastore.retrieveDailyForecastAtLat(location.lat, lon: location.lon, dayCnt: 7) {
+                hourlyWeatherConditions in
+                self?.renderDaily(hourlyWeatherConditions)
                 return
             }
         }
-        
     }
-    
-    // MARK: Current Weather Area
-    private func styleCurrentWeatherView() {
-        lblIcon.textColor = UIColor.whiteColor()
-        lblWeather.font = UIFont.latoLightFontOfSize(20)
-        lblWeather.textColor = UIColor.whiteColor()
-        
-        lblCurrentTemp.font = UIFont.latoLightFontOfSize(96)
-        lblCurrentTemp.textColor = UIColor.whiteColor()
-        
-        lblMaxTemp.font = UIFont.latoLightFontOfSize(18)
-        lblMaxTemp.textColor = UIColor.whiteColor()
-        
-        lblMinTemp.font = UIFont.latoLightFontOfSize(18)
-        lblMinTemp.textColor = UIColor.whiteColor()
-        
-        lblCity.font = UIFont.latoLightFontOfSize(18)
-        lblCity.textColor = UIColor.whiteColor()
-    }
-    
-    private func renderCurrentWeatherView() {
-        lblIcon.attributedText = WIKFontIcon.wiDaySunnyIconWithSize(20).attributedString()
-        lblWeather.text = "Sunny"
-        lblMinTemp.text = "4°"
-        lblMaxTemp.text = "10°"
-        lblCurrentTemp.text = "6°"
-        lblCity.text = "Ho Chi Minh City"
-    }
-    
 }
 
+// MARK: Layout
 private extension ViewController {
-    func renderCurrent(weatherCondition: WeatherCondition){
-        lblIcon.attributedText = iconStringFromIcon(weatherCondition.icon!, size: 20)
-        lblWeather.text = weatherCondition.weather
-        lblMinTemp.text = "\(weatherCondition.minTempCelsius.roundToInt())°"
-        lblMaxTemp.text = "\(weatherCondition.maxTempCelsius.roundToInt())°"
-        lblCurrentTemp.text = "\(weatherCondition.tempCelsius.roundToInt())°"
-        lblCity.text = weatherCondition.cityName ?? ""
+    func layoutView() {
+        constrain(currentWeatherView) { view in
+            // view.top == view.superview!.top + 50
+            view.width == view.superview!.width
+            view.centerX == view.superview!.centerX
+        }
+        
+        constrain(hourlyForecastView, currentWeatherView) { view, view2 in
+            view.top == view2.bottom + 20
+            view.width == view.superview!.width
+            view.centerX == view.superview!.centerX
+        }
+        
+        constrain(daysForecastView, hourlyForecastView) { view, view2 in
+            view.top == view2.bottom
+            view.width == view2.width
+            view.bottom == view.superview!.bottom - 20
+            view.centerX == view.superview!.centerX
+        }
     }
 }
 
-private extension Double {
-    func roundToInt() -> Int{
-        return Int(round(self))
+// MARK: Render
+private extension ViewController {
+    func renderCurrent(currentWeatherConditions: WeatherCondition){
+        currentWeatherView.render(currentWeatherConditions)
+    }
+    
+    func renderHourly(weatherConditions: Array<WeatherCondition>){
+        hourlyForecastView.render(weatherConditions)
+    }
+    
+    func renderDaily(weatherConditions: Array<WeatherCondition>){
+        daysForecastView.render(weatherConditions)
     }
 }
+
